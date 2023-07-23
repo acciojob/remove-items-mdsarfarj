f3c36b39164b7a61903d4e42e4793ba483899003
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-07-23 13:29:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector("input[type='button']");
+    button.addEventListener("click", removeSelectedColor);
+});
+
+function removeSelectedColor() {
+    const colorSelect = document.getElementById("colorSelect");
+    const selectedIndex = colorSelect.selectedIndex;
+
+    if (selectedIndex >= 0) {
+        colorSelect.options[selectedIndex].remove();
+    }
+}
